6965a4886007bcd83249d9c888c1553d5042a019
Oleksandr P <goodwin009@gmail.com>
2023-01-05 17:53:22 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
